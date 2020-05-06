c54557cbcc31515387b4da898adfe703cca185fe
AndyMelR <63148426+AndyMelR@users.noreply.github.com>
2020-05-07 00:32:43 +0200

    Version final romanos a enteros

--- romanos.py
+++ romanos.py
@@ -6,37 +6,39 @@ romanos = {'M':1000,
         'V':5,
         'I':1
     }     
-existen = ['IV','IX','XL','XC','CD','CM']
+existen = ['IV','IX','XL','XC','CD','CM'] #combinaciones aceptadas de menor a mayor y que restan
 
 def romano_a_entero(numero_romano):
     if numero_romano == '':
         return 'Error en formato'
 
-    if len(numero_romano)>3:
-        return 'Error en formato'
-
     entero = 0 
-    numRepes = 1
+    numRepes = 1 #guardamos número de repeticiones
     letraAnt = ''
-    for letra in numero_romano:
-        
-        if letra == letraAnt and numRepes == 3:
-            return 'Error en formato'
-        elif letra == letraAnt :
-            numRepes += 1
-        else:
-            numRepes = 1
+    fueResta = False
+    for letra in numero_romano:#bucle para recorrer cada letra del digito introducido
 
         if letra in romanos:
             if letraAnt == '' or  romanos[letraAnt] >= romanos[letra]:    
-                entero += romanos [letra]
+                entero += romanos[letra] #acumulamos
+                fueResta = False #primera resta
             else:
-                if letraAnt + letra in existen:
-                     entero = entero - romanos[letraAnt] * 2 + romanos [letra]
+                if letraAnt + letra in existen and numRepes < 2 and not fueResta: #si estan en existen
+                     entero = entero - romanos[letraAnt] * 2 + romanos[letra] 
+                     fueResta = True 
+                else:
+                    return 'Error en formato'
         else:
             return 'Error en formato'
+           
+        if letra == letraAnt and numRepes == 3:
+            return 'Error en formato'
+        elif letra == letraAnt : #validamos letra Ant en este bucle
+            numRepes += 1 #acumulamos letras hasta llegar a 3
+        else:
+            numRepes = 1
         
-        letraAnt = letra
+        letraAnt = letra #siempre debe ser lo último. Las igual para poder compararlas y saber si ya ha salido
             
     return entero
 
